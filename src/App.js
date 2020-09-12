5fbf7d670b164001a67129a98dc49776a2229bfd
Thomas Fogg <thomasfogg@Thomass-Mini.attlocal.net>
2020-09-12 14:12:51 -0500

    needs styling

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React, { useEffect, useState } from "react"
-import ReactDOM from "react-dom"
 import { BrowserRouter, Switch, Route } from "react-router-dom"
-import "./App.css"
 import { ThemeProvider } from "react-jss"
 import theme from "./Styles/theme"
 import HeaderLoggedOut from "./components/HeaderLoggedOut"
